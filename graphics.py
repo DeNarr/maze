83e8414524af4f94a581bb336f54a1e331fc9a4b
DeNarr <rjsrz9@gmail.com>
2025-04-09 11:05:42 -0500

    Cell draw_move

--- graphics.py
+++ graphics.py
@@ -53,6 +53,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -68,4 +70,20 @@ class Cell:
             self._win.draw_line(line)
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)       
+            self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -7,17 +7,20 @@ def main():
     c.has_left_wall = False
     c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.has_right_wall = False
+    c2.draw(125, 125, 200, 200)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c3 = Cell(win)
+    c3.has_bottom_wall = False
+    c3.draw(225, 225, 250, 250)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c4 = Cell(win)
+    c4.has_top_wall = False
+    c4.draw(300, 300, 500, 500)
+
+    c.draw_move(c2, True)
+    c2.draw_move(c3)
     win.wait_for_close()
 
 
