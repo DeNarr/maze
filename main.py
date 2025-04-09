6598b5a7fbb82091d9b437b1f4b2824a53a650f8
DeNarr <rjsrz9@gmail.com>
2025-04-09 10:11:55 -0500

    Drawing some lines

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from graphics import Window
+from graphics import *
 
 
 def main():
     win = Window(800, 600)
+    point1 = Point(20, 20)
+    point2 = Point(270, 70)
+    line = Line(point1, point2)
+    win.draw_line(line, "black")
+    line2 = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 
